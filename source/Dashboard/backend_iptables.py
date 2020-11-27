d04f29d1f2d62b1300e32625a0755a004040ef46
pyDeb <sadeghi2@uwindsor.ca>
2020-11-27 04:34:36 +0000

    requirements.txt created

---
+++ source/Dashboard/backend_iptables.py
@@ -0,0 +1,218 @@
+import subprocess as sb
+import logging
+
+
+#OBJECT_ADDED_SUCCESSFULLY = 0
+#ENTRY_IS_ALREADY_ADDED = 1
+#ERROR_WHILE_ADDING_ENTRY = 2
+#SET_CREATED_SUCCESSFULLY = 3
+#ERROR_WHILE_CREATING_SET = 4
+#POLICY_WAS_ADDED_SUCCESSFULLY = 5
+#ERROR_WHILE_ADDING_POLICY = 6
+#OBJECT_DELETED_SUCCESSFULLY = 7
+#ERROR_WHILE_DELETING_OBJECT = 8
+#IPSET_DESTROYED_SUCCESSFULLY = 9
+#ERROR_WHILE_DESTROYING_IPSET =10
+
+
+
+def getAllSets():
+	sets_list = []
+	out = sb.Popen(['sudo', 'ipset', 'list'], shell = False, stdout = sb.PIPE)
+	res, err = out.communicate()
+	if res:
+		num_of_sets = len(res.decode().split('Name: ')) - 1
+		for i in range(1, num_of_sets + 1):
+			sets_dict = {}
+			name = res.decode().split("Name: ")[i].split('\n')[0]
+			sets_dict['set_name'] = name
+			
+			number_of_entires = int(res.decode().split('Number of entries: ')[i].split('\n')[0])
+			addresses_list = []
+			for j in range(number_of_entires):
+				addresses_list.append(res.decode().split('Number of entries: ')[i].split('\n')[2+j])
+
+			sets_dict['addresses'] = addresses_list
+			sets_list.append(sets_dict)
+
+	return sets_list
+
+
+def ipsetLogging(logger):
+	fhl = logging.FileHandler('ipset.log')
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+	logger.setLevel(logging.DEBUG)
+
+	ch = logging.StreamHandler()
+	logger.addHandler(ch)
+	ch.setFormatter(formatter)
+	ch.setLevel(logging.DEBUG)
+
+	logger.addHandler(fhl)
+	fhl.setFormatter(formatter)
+	fhl.setLevel(logging.DEBUG)
+
+
+def getOneIpsetEntries(set_name):
+	all_sets = getAllSets()
+	for ipset in all_sets:
+		if set_name == ipset['set_name']:
+			return ipset['addresses']
+
+
+#it deletes a single entry from a special ipset
+def deleteIpsetEntry(set_name, entry_value):
+	out = sb.Popen(['sudo', 'ipset', 'del', set_name, entry_value], shell = False, stderr = sb.PIPE)
+	res, err = out.communicate()
+	if not err:
+		logger.debug('Entry %s was deleted from set %s successfully' % (entry_value, set_name))
+		#sb.Popen(['/etc/ipset_save.sh'], shell = False)
+		sb.call('sudo ipset save > /etc/Firewall/ipsetSave.conf', shell = True)
+	else:
+		logger.debug('%s' %err)
+
+
+
+
+#it destroys an ipset
+def destroyIpset(set_name):
+	out = sb.Popen(['sudo', 'ipset', 'destroy', set_name], shell = False, stderr = sb.PIPE)
+	res, err = out.communicate()
+	if err:
+		logger.debug("%s" %err)
+	else:
+		logger.debug('IPset %s was destroyed completely' %set_name)
+		sb.call('sudo ipset save > /usr/local/etc/ipsetSave.conf', shell = True)
+		#sb.Popen(['/etc/ipset_save.sh'], shell = False)
+
+
+
+
+#it edits a single entry
+def editIpsetEntry(old_set_name, new_set_name, new_value, old_value, new_type="hash:net", old_type="hash:net"):
+	if old_set_name == new_set_name  and new_type == old_type:
+		deleteIpsetEntry(old_set_name, old_value)
+		addIpsetEntry(new_set_name, new_value)
+		logger.debug("Entry in set %s was replaced with %s in set %s" %(old_set_name, new_value, new_set_name))	
+		sb.call('sudo ipset save > /usr/local/etc/ipsetSave.conf', shell = True)
+		
+	else:
+		if new_type == "iprange" or new_type == "fqdn":
+			createIpset(new_set_name, "hash:ip")
+		if new_type == "hash:net":
+			createIpset(new_set_name, "hash:net")
+		if new_type == "mac":
+			createIpset(new_set_name, "hash:mac")
+		addIpsetEntry(new_set_name, new_value)
+	
+	logger.debug("Entry with value of {%s} in set (%s) was replaced with {%s} in set (%s)" %(old_value, old_set_name, new_value, new_set_name))	
+	sb.call('sudo ipset save > /usr/local/etc/ipsetSave.conf', shell = True)
+	return True
+
+
+def checkExistingIpset(set_name):
+	list_output = sb.Popen(['sudo', 'ipset', 'list'], shell = False, stdout = sb.PIPE)
+	grep_output = sb.Popen(['grep', '-w', set_name], shell = False, stdin = list_output.stdout, stdout = sb.PIPE)
+	result, err = grep_output.communicate()
+	if result:
+		return True
+	else:
+		return False
+
+
+def createIpset(set_name, set_type="hash:net"):
+	out = sb.Popen(['sudo', 'ipset', '-N', set_name, set_type, 'comment'], shell = False, stderr = sb.PIPE)
+	res,err = out.communicate()
+	if err:
+		logger.debug('%s' %err)
+	else:
+		logger.debug('IPset named %s of type %s was created successfully' % (set_name, set_type))
+		sb.call('sudo ipset save > /etc/Firewall/ipsetSave.conf', shell = True)
+		#sb.Popen(['/etc/ipset_save.sh'], shell = False)
+
+
+
+
+def addIpsetEntry(set_name, entry_value ,comment=""):
+	out = sb.Popen(['sudo', 'ipset', '-A', set_name, entry_value, '--exist'], shell = False, stderr = sb.PIPE)
+	out.wait()
+	err = out.communicate()
+	if err[1]:
+		logger.debug('%s' %err[1])
+		return 1
+	else:
+		logger.debug('Entry %s was added successfully in set %s' %(entry_value, set_name))
+		#sb.Popen(['/etc/ipset_save.sh'], shell = False)
+		return 0
+
+
+		
+
+def addRule(policy_name, address_set, interface, port, protocol, action, is_input):
+	
+	if is_input:
+		proc = sb.Popen(['sudo', 'iptables', '-A', 'INPUT', '-m', 'set', '--match-set', address_set, 'src', '-p', protocol, '--sport', 
+			port, '-m', 'comment', '--comment', policy_name, '-j',  action], shell = False, stderr = sb.PIPE)
+		proc.wait()
+		err = proc.communicate()
+		if err[1]:
+			logger.debug('%s' %err[1])
+			return 6
+
+
+	else:
+		proc = sb.Popen(['sudo', 'iptables', '-A', 'OUTPUT', '-m', 'set', '--match-set', address_set, 'dst', '-p', protocol, '--dport', 
+			port, '-m', 'comment', '--comment', policy_name, '-j', action ], shell = False, stderr = sb.PIPE)
+		proc.wait()
+		err = proc.communicate()
+		if err[1]:
+			logger.debug('%s' %err[1])
+			return 6
+
+
+
+
+
+def getAllRules(is_input):
+	rules_list = []
+
+	if is_input:
+		proc = sb.Popen(['sudo', 'iptables', '-nvL', 'INPUT'], shell=False, stdout=sb.PIPE, stderr = sb.PIPE)
+		
+	else:
+		proc = sb.Popen(['sudo', 'iptables', '-nvL', 'OUTPUT'], shell=False, stdout=sb.PIPE, stderr = sb.PIPE)
+
+	proc.wait()
+	output = proc.communicate()[0]
+
+	splitted_output = output.decode().split('\n')[2: len(output.decode().split('\n')) - 1]
+	for val in splitted_output:
+		rule_dic = {}
+		space_list = val.split(' ')
+		space_list = list(filter(None, space_list))
+		print(space_list)
+		rule_dic['target'] = space_list[2]
+		rule_dic['protocol'] = space_list[3]
+		rule_dic['source_interface'] = space_list[5]
+		rule_dic['set_name'] = space_list[10]
+		rule_dic['policy_name'] = space_list[15]
+		rule_dic['port'] = space_list[13].split(':')[1]
+		rules_list.append(rule_dic)
+
+	return rules_list
+
+
+def deleteRule(is_input, index):
+	if is_input:
+		proc = sb.Popen(['sudo', 'iptables', '-D', 'INPUT', str(index)], shell=False, stdout=sb.PIPE, stderr = sb.PIPE)
+	else:
+		proc = sb.Popen(['sudo', 'iptables', '-D', 'OUTPUT', str(index)], shell=False, stdout=sb.PIPE, stderr = sb.PIPE)
+
+	
+
+def main():
+	print(getAllSets())
+
+
+if __name__ == '__main__':
+	main()
--- source/Dashboard/views.py
+++ source/Dashboard/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from . import backend_iptables as back
 
 # Create your views here.
+
+
+def addressbag(request):
+    all_ipsets = back.getAllSets()
+    return render(request, 'index.html', {'bag_list': all_ipsets})
+
+
+def addaddresstobag(request):
+    if request.method == 'POST':
+        bagname = request.POST['bagname']
+        ip1 = request.POST['ip1']
+        ip2 = request.POST['ip2']
+        ip3 = request.POST['ip3']
+        ip4 = request.POST['ip4']
+        ip5 = request.POST['ip5']
+
+        entry_value = ip1 + '.' + ip2 + '.' + ip3 + '.' ip4 + '/' + ip5
+
+        # check if bagname exists
+        if not back.checkExistingIpset(bagname):
+            # create new bag with the bagname && add the ip to the bag
+            back.createIpset(bagname)
+        back.addIpsetEntry(bagname, entry_value)
+
+        # UPDATE: bag_list && ip_list (list of ips in current baganme)
+        all_ipsets = getAllSets()
+        return render(request, 'fireNet/index.html', {'bag_list': all_ipsets, 'bagname': bagname})
+
+
+def removeaddressfrombag(request):
+    if request.method == 'POST':
+        bag_list = {}
+        bagname = request.POST['bagname']
+        ip1 = request.POST['ip1']
+        ip2 = request.POST['ip2']
+        ip3 = request.POST['ip3']
+        ip4 = request.POST['ip4']
+        ip5 = request.POST['ip5']
+        entry_value = ip1 + '.' + ip2 + '.' + ip3 + '.' ip4 + '/' + ip5
+
+        # remove and updating the address
+        deleteIpsetEntry(bagname, entry_value)
+        # updated ip list and bagname should be sent
+        ip_list = back.getOneIpsetEntries(bagname)
+        return render(request, 'fireNet/index.html', {'bag_list': bag_list, 'ip_list': ip_list, 'bagname': bagname})
+
+
+def modifybag(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            bag_list = {}
+            input_name = request.POST['bagname']
+
+            # remove and updating the address
+            print(request.body)
+
+            # updated ip list and bagname should be sent
+            ip_list = []
+            bagname = ""
+            return render(request, 'fireNet/index.html', {'bag_list': bag_list, 'ip_list': ip_list, 'bagname': bagname})
+
+
+def inputconf(request):
+    rule_list = back.getAllRules(True)
+    return render(request, 'INPUTconf.html', {'rule_list': rule_list})
